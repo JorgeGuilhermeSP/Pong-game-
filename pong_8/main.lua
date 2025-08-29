e1f66f52bf6fb60dc0712b1d3dbf95562f2281dc
Jorge Guilherme Souza Pinheiro <jorgeguilhermesp@gmail.com>
2025-08-29 18:39:56 -0300

    Add files via upload

---
+++ pong_8/main.lua
@@ -0,0 +1,184 @@
+push = require "push"
+Class = require 'class'
+require 'Paddle'
+require 'Ball'
+
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+-- velocidade do travessão
+PADDLE_SPEED = 200
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Pong')
+
+    -- aleatoriedade baseada em tempo
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- fonte do placar
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+    love.graphics.setFont(smallFont)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+
+    -- placar
+    player1Score = 0
+    player2Score = 0
+
+    -- posição do travessão no eixo vertical
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+
+    -- posição da bola
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
+    -- estado de jogo
+    gameState = 'start'
+end
+
+-- movimento dos travessões
+function love.update(dt)
+
+if gameState == 'play' then
+    if ball:collides(player1) then
+        ball.dx = -ball.dx * 1.03
+        ball.x = player1.x + 5
+    
+    if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+    else
+        ball.dy = math.random(10, 150)
+
+    end
+end
+
+if ball:collides(player2) then
+        ball.dx = -ball.dx * 1.03
+        ball.x = player2.x - 4
+    
+    if ball.dy < 0 then
+        ball.dy = -math.random(10, 150)
+    else
+        ball.dy = math.random(10, 150)
+
+    end
+end
+
+if ball.y <= 0 then
+    ball.y = 0
+    ball.dy = -ball.dy
+end
+if ball.y >= VIRTUAL_HEIGHT - 4 then
+    ball.y = VIRTUAL_HEIGHT - 4
+    ball.dy = -ball.dy
+end
+end
+
+-- Lógica do placar
+if ball.x < 0 then
+    servingPlayer = 1
+    player2Score = player2Score + 1
+    ball:reset()
+    gameState = 'start'
+end
+if ball.x > VIRTUAL_WIDTH then
+    servingPlayer = 2
+    player1Score = player1Score + 1
+    ball:reset()
+    gameState = 'start'
+end
+
+-- jogador 1
+if love.keyboard.isDown('w') then
+    player1.dy = -PADDLE_SPEED
+elseif love.keyboard.isDown('s') then
+    player1.dy = PADDLE_SPEED
+else 
+    player1.dy = 0
+    
+end
+
+
+-- jogador 2
+if love.keyboard.isDown('up') then
+    player2.dy = -PADDLE_SPEED
+elseif love.keyboard.isDown('down') then
+    player2.dy = PADDLE_SPEED
+else 
+    player2.dy = 0
+    
+end
+
+-- inicio de jogo
+if gameState == 'play' then
+    ball:update(dt)
+end
+    player1:update(dt)
+    player2:update(dt)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+
+ -- trasição do estado de jogo
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else 
+            gameState = 'start'
+            ball:reset()
+        end
+    end
+end
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    love.graphics.setFont(smallFont)
+
+    -- renderizar estado de jogo
+    if gameState == 'start' then
+            love.graphics.printf ('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')  
+    else    love.graphics.printf ('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    
+    end
+    -- renderizar placar
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+-- retangulo esquerdo
+    player1:render()
+-- retangulo direito
+    player2:render()
+ -- bola
+    ball:render()
+
+    displayFPS()
+
+ push:apply('end')
+end
+
+    -- FPS
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255/255, 0, 255/255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)    
+end
+
+
+
